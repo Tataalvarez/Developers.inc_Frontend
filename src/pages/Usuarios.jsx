f1856f92a67d5b77624b9e8567956095fac3c5ed
richmon43 <richmon430@gmail.com>
2021-11-11 01:46:36 -0500

    conecting server with client is ok

--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { useQuery } from "@apollo/client";
+import React, { Fragment } from "react";
+import { GET_USERS } from "../graphql/login";
+import Usuario from "./Usuario";
 
 const Usuarios = () => {
-  return <div>Gestion Usuarios</div>;
+  const { loading, data } = useQuery(GET_USERS);
+  function renderUsers() {
+    return data.getUsers.map((user) => {
+      return <Usuario key={user.id} name={user.nombre}></Usuario>;
+    });
+  }
+
+  return (
+    <Fragment>
+      {loading ? <p>Cargando...</p> : <ul>{renderUsers()}</ul>}
+    </Fragment>
+  );
 };
 
 export default Usuarios;
