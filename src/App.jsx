06936148d47709e60bc94426a3f353022ee91503
richmon43 <richmon430@gmail.com>
2021-11-25 17:25:55 -0500

    agregando la navegacion y mejorando la vista de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import React, { useState, useEffect  } from 'react';
-import PrivateLayout from 'layouts/PrivateLayout';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
+import React, { useState, useEffect } from "react";
+import PrivateLayout from "layouts/PrivateLayout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import Navbar from "./components/Navbar.jsx";
-import { UserContext } from 'context/userContext';
-import Index from 'pages/Index';
-import Perfil from 'pages/Perfil';
-import Usuarios from 'pages/Usuarios';
-import Proyectos from 'pages/Proyectos';
+import { UserContext } from "context/userContext";
+import Index from "pages/Index";
+import Perfil from "pages/Perfil";
+import Usuarios from "pages/Usuarios";
+import Proyectos from "pages/Proyectos";
 // import Login from 'pages/Login';
 // import Registro from 'pages/Registro';
-import Auth from 'pages/auth/Auth.jsx';
-import 'styles/globals.css';
+import Auth from "pages/auth/Auth.jsx";
+import "styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import client from "config/apollo.js";
 
 // import PrivateRoute from 'components/PrivateRoute';
 
 function App() {
-  const [userData, setUserData ] = useState({});
+  const [userData, setUserData] = useState({});
 
   const [showNav, setShowNav] = useState(true);
 
-   /* Oculta la navbar en las rutas que no tenga que estar */
-   const toggleNav = (options) => {
+  /* Oculta la navbar en las rutas que no tenga que estar */
+  const toggleNav = (options) => {
     setShowNav(options);
   };
 
@@ -39,28 +41,25 @@ function App() {
   });
 
   return (
-    <Auth0Provider
-      domain=''
-      clientId=''
-      redirectUri=''
-      audience=''
-    >
-      <UserContext.Provider value={{ userData, setUserData }}>
-        <BrowserRouter>
-        <Navbar showNav={(state) => toggleNav(state)} />
-          <Routes>
-            <Route path='/' element={<PrivateLayout />}>
-              <Route path='' element={<Index />} />
-              <Route path='perfil' element={<Perfil />} />
-              <Route path='usuarios' element={<Usuarios />} />
-              <Route path='proyectos' element={<Proyectos/>} />
-              <Route path='login' element={<Auth />} />
-              {/* <Route path='registro' element={<Registro />} /> */}
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </UserContext.Provider>
-    </Auth0Provider>
+    <ApolloProvider client={client}>
+      <Auth0Provider domain="" clientId="" redirectUri="" audience="">
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <Router>
+            <Navbar showNav={(state) => toggleNav(state)} />
+            <Routes>
+              <Route path="/" element={<PrivateLayout />}>
+                <Route path="" element={<Index />} />
+                <Route path="perfil" element={<Perfil />} />
+                <Route path="usuarios" element={<Usuarios />} />
+                <Route path="proyectos" element={<Proyectos />} />
+                <Route path="login" element={<Auth />} />
+                {/* <Route path='registro' element={<Registro />} /> */}
+              </Route>
+            </Routes>
+          </Router>
+        </UserContext.Provider>
+      </Auth0Provider>
+    </ApolloProvider>
   );
 }
 
