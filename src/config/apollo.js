c04cbb49c7a4e15ad5e893355bfe2fcbd8b1530a
Tataalvarez <tatianaalvarezmartinez@gmail.com>
2021-12-18 00:23:59 -0500

    config heroku

--- src/config/apollo.js
+++ src/config/apollo.js
@@ -1,8 +1,9 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+const PORT = process.env.PORT || 3000;
 // import fetch from 'node-fetch';
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/"
+  uri: "https://git.heroku.com/vast-cliffs-02417.git"
 });
 
 const client = new ApolloClient({
